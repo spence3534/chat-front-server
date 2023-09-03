549ce135cda34dcaaa9a5c9095b679faaf2ce34f
Li <spence.li@momodoc.ai>
2023-09-03 17:50:46 +0800

    init

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+/*
+ * @Author: Li
+ * @Date: 2023-08-30 13:26:42
+ * @LastEditTime: 2023-08-30 13:52:21
+ * @Description: 
+ */
+
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>index页面</div>
+  )
+}
